8c37c68576c5685c51fc1e01cedbce09e42949f8
kmigel <annkri2428@gmail.com>
2025-12-27 00:01:05 +0100

    Fix typo

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -3,10 +3,10 @@ const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
     let header = req.headers["authorization"];
-    if(!header) return res.status("401").json({error: "No token provided"});
+    if(!header) return res.status(401).json({error: "No token provided"});
 
     let token = header.split(" ")[1];
-    if(!token) return res.status("401").json({error: "No token provided"});
+    if(!token) return res.status(401).json({error: "No token provided"});
 
     try {
         let decoded = jwt.verify(token, process.env.JWT_SECRET);
